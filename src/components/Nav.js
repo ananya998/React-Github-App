2048e960d8b5332f44d6dc0e3857e0dfc90dde48
Ananya Trivedi <ananyatrivedi070@gmail.com>
2021-12-04 15:06:54 +0530

    Add files via upload

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Conatiner from './Container';
+import Form from './Form';
+const Nav = () => {
+    return(
+
+        <nav>
+            <Conatiner>
+                <Form />
+            </Conatiner>
+        </nav>
+    )
+};
+
+export default Nav;
